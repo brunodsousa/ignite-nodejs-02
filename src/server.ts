ca4c7d35c12cde3d2b9abe3e6b40a61ebbdcf8c1
Bruno Sousa <80365075+brunodsousa@users.noreply.github.com>
2023-05-31 22:40:01 -0300

    Fastify cookie import and register

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import fastify from "fastify";
+import cookie from "@fastify/cookie";
 import { env } from "./env";
 import { usersRoutes } from "./routes/users";
 import { mealsRoutes } from "./routes/meals";
 
 const app = fastify();
 
+app.register(cookie);
 app.register(usersRoutes, {
   prefix: "users",
 });
